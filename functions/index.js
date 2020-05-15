1d3d2acd194adf19b9a3598c3bcaeb99e3335d98
Ilgiz Mustafin <ilgimustafin@gmail.com>
2020-05-15 20:40:56 +0300

    Add notification service

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,47 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.onMessageCreate = functions
+  .database
+  .ref('/chats/{room}/{message}')
+  .onCreate(async (snapshot, context) => {
+    const { room } = context.params;
+    const message = snapshot.val();
+
+    const tokens = await admin.database().ref('/tokens/{userId}').once('value');
+
+    if (!tokens.hasChildren()) {
+      return console.log('No tokens known');
+    }
+
+    const payload = {
+      notification: {
+        title: 'New message',
+        body: message.content
+      }
+    };
+
+    const tokenStrings = Object.keys(tokens.val());
+
+    const response = await admin.messaging().sendToDevice(tokenStrings, payload);
+
+
+    // Remove stale tokens
+
+    const staleTokenRemovePromises = [];
+
+    response.results.forEach((result, index) => {
+      const { error } = result;
+      if (error) {
+        console.log('Error sending notification to', tokenStrings[index], error);
+
+        if (error.code === 'messaging/invalid-registration-token' ||
+            error.code === 'messaging/registration-token-not-registered') {
+          staleTokenRemovePromises.push(tokens.ref.child(tokenStrings[index]).remove());
+        }
+      }
+    });
+
+    return Promise.all(staleTokenRemovePromises);
+  });
